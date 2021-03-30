e38d9767d101a5aa8e238234de29e1de5a6c1aa7
Mel1207 <meljohnpualon@gmail.com>
2021-03-31 00:23:01 +0800

    set function for active states

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,45 @@
-new fullpage ('#fullPage', {
-    autoScrolling: true,
-    licenseKey: null
-})
+// new fullpage ('#fullPage', {
+//     autoScrolling: true,
+//     licenseKey: null
+// })
+
+// const links = document.querySelectorAll('.link');
+// const sections = document.querySelectorAll('.vh-container');
+
+
+// function changeLinkState () {
+//     let index = sections.length;
+
+//     white(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+//         links.forEach((link) => link.classList.remove('active'));
+//         links[index].classList.add('active');
+    
+// }
+
+// changeLinkState();
+// window.addEventListener('scroll', changeLinkState);
+
+
+const links = document.getElementsByClassName('.link');
+const sections = document.getElementsByClassName('.vh-container');
+
+function changeLinkState() {
+  let index = sections.length;
+
+  while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  
+  links.forEach((link) => link.classList.remove('active'));
+  links[index].classList.add('active');
+}
+
+changeLinkState();
+window.addEventListener('scroll', changeLinkState);
+
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    console.log(scroll)
+
+});
+
+event.preventDefault();
+
