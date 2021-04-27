da43cabbd909f6545145781b3879681ac3d3fd41
Mel <meljohnpualon@gmail.com>
2021-04-28 06:00:19 +0800

    added sidenav and event listener for mobile icon

--- public/js/app.js
+++ public/js/app.js
@@ -1,45 +1,12 @@
-// new fullpage ('#fullPage', {
-//     autoScrolling: true,
-//     licenseKey: null
-// })
-
-// const links = document.querySelectorAll('.link');
-// const sections = document.querySelectorAll('.vh-container');
-
-
-// function changeLinkState () {
-//     let index = sections.length;
-
-//     white(--index && window.scrollY + 50 < sections[index].offsetTop) {}
-//         links.forEach((link) => link.classList.remove('active'));
-//         links[index].classList.add('active');
-    
-// }
-
-// changeLinkState();
-// window.addEventListener('scroll', changeLinkState);
-
-
+const burgerMenu = document.querySelector('.burger-menu');
 const links = document.getElementsByClassName('.link');
 const sections = document.getElementsByClassName('.vh-container');
 
-function changeLinkState() {
-  let index = sections.length;
 
-  while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
-  
-  links.forEach((link) => link.classList.remove('active'));
-  links[index].classList.add('active');
+// Event Listenters
+burgerMenu.addEventListener('click', openSideNav);
+
+
+function openSideNav () {
+  console.log('button click')
 }
-
-changeLinkState();
-window.addEventListener('scroll', changeLinkState);
-
-window.addEventListener("scroll", (event) => {
-    let scroll = this.scrollY;
-    console.log(scroll)
-
-});
-
-event.preventDefault();
-
