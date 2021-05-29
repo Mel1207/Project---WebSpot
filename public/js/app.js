9aa6be7c20c5321d4d0d5336d501269a2ecae9a4
Mel <meljohnpualon@gmail.com>
2021-05-30 01:53:07 +0800

    added remove class functionality for close icon

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,18 @@ const burgerMenu = document.querySelector('.burger-menu');
 const links = document.getElementsByClassName('.link');
 const sections = document.getElementsByClassName('.vh-container');
 const sideNav = document.querySelector('.sidenav-overlay');
+const closeNav = document.querySelector('.close-icon');
 
 
 // Event Listenters
 burgerMenu.addEventListener('click', openSideNav);
+closeNav.addEventListener('click', closeSideNav);
 
 
 function openSideNav () {
   sideNav.classList.toggle('active')
+}
+
+function closeSideNav () {
+  sideNav.classList.remove('active')
 }
